36bf248f2d76407fda56941f26481aac7e16ba3d
euimokna <euimok@gmail.com>
2022-09-30 12:41:44 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+    source  = "app.terraform.io/DevOpsProduct/network/google//modules/vpc"
+    version = "~> 2.0.0"
+
+    project_id   = "var.project"
+    network_name = "gaurav-network"
+    
+    subnets = [
+      {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+      }
+    ]
+}
